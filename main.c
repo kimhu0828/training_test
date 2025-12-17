e80f88bbe161644ff1254b057159c53ca15e9061
HUKIM <hukim@infuse.co.kr>
2025-12-17 15:47:14 +0900

    sub func implement

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+int sub(int a, int b)
+{
+	return a - b;
+}
+
 int main(void)
 {
+	static int a = 5, b = 3, result;
+	
+	result = sub(a, b);
+	
+	printf("sub result = %d\n", result);
+	
 	return 0;
 }
