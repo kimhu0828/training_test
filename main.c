2b66a822e88da799846a627ee2f340b220e2086a
HUKIM <hukim@infuse.co.kr>
2025-12-17 15:35:57 +0900

    add func implement

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
 int main(void)
 {
+	static int a = 5;
+	static int b = 5;
+	static int result;
+	
+	result = add(a,b);
+	
+	printf(%d + %d = "%d\n", a, b, result);
+	
 	return 0;
 }
